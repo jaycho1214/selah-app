de30fcbc01a61fe6da775b133bcbda076deadf3e
Jay Cho <jaycho1214@gmail.com>
2026-02-02 11:23:36 +0900

    feat(02-01): install Better Auth dependencies and configure Metro
    - Add expo-secure-store for encrypted token storage
    - Add better-auth and @better-auth/expo for auth client
    - Enable unstable_enablePackageExports in Metro for package exports

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Required for @better-auth packages which use package exports
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withNativeWind(config, { input: './global.css' });
